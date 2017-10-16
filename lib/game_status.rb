1ef09764007d098ff92004cec4bcb7282e557a87
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-16 16:56:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,6 @@ def won?(board)
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
-
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
@@ -28,7 +27,7 @@ def won?(board)
     if (position_1 == "X" && position_2 == "X" && position_3 == "X")
       return win_combination
     else
-      return false
+      false
     end
   end
 end
